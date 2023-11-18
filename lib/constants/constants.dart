51e8760f0ce414012fe711413cb025fef41b7cd4
khalid <khalidcourses2022@gmail.com>
2023-11-18 17:06:52 +0100

    splash screen added , and the necessary folders.

---
+++ lib/constants/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const kAppColor = Color.fromARGB(255, 238, 158, 197);
+const kCardBgColor =  Color.fromARGB(230, 255, 203, 227);
+
+const kDefaultPadding = 20.0;
+
+const kTitle = "Empowering Women: Understanding and Confronting Breast Cancer" ;
+const kParagraph = "Breast cancer is a formidable adversary that affects millions of women worldwide. It is a complex and multifaceted disease, characterized by the uncontrolled growth of malignant cells in the breast tissue. The impact of breast cancer goes beyond its physical manifestations, touching the lives of patients, their families, and communities at large. Early detection through regular screenings, advances in medical research, and innovative treatment options have significantly improved the prognosis for many patients.";
---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:medical_app_final/constants/constants.dart';
+import 'package:medical_app_final/pages/home_page.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kAppColor,
+      body: Center(child: Image.asset('images/splash_img.png')),
+    );
+  }
+}
